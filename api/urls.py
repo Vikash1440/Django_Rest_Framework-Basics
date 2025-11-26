bc9e923dd883a72ed1fdd5e454bfe2733e518624
Vikash1440 <meenavikash34262@gmail.com>
2025-11-26 15:28:18 +0530

    implement generics and viewset

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from .import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('employees',views.EmployeeView,basename='employees')
+
 
 urlpatterns = [
     
     path('students/',views.studentsView,name='studentsView'),
     path('student/<int:pk>/',views.studentDetailView,name='studentDetailView'),
-    path('employees/',views.EmployeeView.as_view(),name='EmployeeView'),
-    path('employees/<int:pk>/',views.EmployeeDetailView.as_view(),name='EmployeeDetailView'),
+    # path('employees/',views.EmployeeView.as_view(),name='EmployeeView'),
+    # path('employees/<int:pk>/',views.EmployeeDetailView.as_view(),name='EmployeeDetailView'),
+    
+    path('',include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 # from django.http import JsonResponse
 from students.models import Student
 from .serializers import StudentSerializer,EmployeeSerializer
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
-from rest_framework import generics,mixins
+from rest_framework import generics,mixins,viewsets
 
 
 # function based views for student table
@@ -113,11 +113,51 @@ def studentDetailView(request,pk):
 
 
 # more simplified generic class based views for employee table
-class EmployeeView(generics.ListAPIView,generics.CreateAPIView):
+# class EmployeeView(generics.ListCreateAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+
+
+# class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='pk'
+
+"""
+# ViewSet class based views for employee table
+class EmployeeView(viewsets.ViewSet):
+    def list(self,request):
+        queryset=Employee.objects.all()
+        serializer=EmployeeSerializer(queryset,many=True)
+        return Response(serializer.data)
+
+    def create(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+    
+    def retrieve(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
+        serializer=EmployeeSerializer(employee)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def update(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
+        serializer=EmployeeSerializer(employee,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+"""
+
+# simplyfia class based views
+class EmployeeView(viewsets.ModelViewSet):
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
-
-
-class EmployeeDetailView(generics.RetrieveAPIView):
-    pass 
-
