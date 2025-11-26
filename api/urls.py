e4a1fb576bc401d96797f4eb40acdb55165fdc87
Vikash1440 <meenavikash34262@gmail.com>
2025-11-26 10:50:08 +0530

    implement mixins class based view

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .import views
 urlpatterns = [
     
     path('students/',views.studentsView,name='studentsView'),
-    path('student/<int:pk>/',views.studentDetailView,name='studentDetailView')
-    
+    path('student/<int:pk>/',views.studentDetailView,name='studentDetailView'),
+    path('employees/',views.EmployeeView.as_view(),name='EmployeeView'),
+    path('employees/<int:pk>/',views.EmployeeDetailView.as_view(),name='EmployeeDetailView'),
 ]
